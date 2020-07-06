731e8037f0a2ab06cb677dea9c6e62128985d0b6
chackman okuchi <bisufa@svk.jp>
2020-07-06 17:36:57 +0900

    読み込み画像のwidth取得

--- VoiroGokko.js
+++ VoiroGokko.js
@@ -4,10 +4,12 @@ function Tachie(Command) {
     //ƒXƒLƒ“‰Šúİ’è
     var command = Command;
     var y = 0; 
+    var width = 0;
     this.skinimage = new createjs.Bitmap();//‹ó‰æ‘œ
     this.skinloader = new createjs.ImageLoader('http://localhost/skins/' + command + '.png', false);
     this.skinloader.addEventListener("complete", (function (event) {
         this.skinimage.image = new createjs.Bitmap(event.result).image;
+        //console.log(this.skinimage.image.width);
     }).bind(this), false);//’ˆÓ
     this.skinloader.load();//’ˆÓ
 };
