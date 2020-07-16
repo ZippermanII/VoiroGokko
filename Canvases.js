10e9191ba83a194a087342ba9b292a4201431e3a
ZippermanII <ababa@ex.com>
2020-07-17 00:30:19 +0900

    styleからの呼び出し画像の反転に対応

--- Canvases.js
+++ Canvases.js
@@ -6,17 +6,35 @@ class Canvases {
         this.layer = 10000;
     }
 
+    StyleDeclarationSetTransform(style, value) {
+    var list = [
+        "transform",
+        "webkitTransform",
+        "MozTransform",
+        "msTransform",
+        "OTransform"
+    ];
+    var i;
+    var num = list.length;
+    for (i = 0; i < num; i++) {
+        if (style[list[i]] !== undefined) {
+            style[list[i]] = value;
+            return true;
+        }
+    }
+        return false;
+}
+
     //新規キャンバスを作成
     Create(user_id, canvas_count) {
         var new_canvas = document.createElement("canvas");
-        var ctx = new_canvas.getContext("2d");
+        var style = new_canvas.style;
         new_canvas.setAttribute("data-position", canvas_count);
         new_canvas.id = user_id;
         new_canvas.width = 1920;
         new_canvas.height = 1080;
         new_canvas.style.position = "absolute";
         new_canvas.style.zIndex = this.layer;
-        ctx.scale(-1, 1);
         //ポジションにあわせて描画位置を調整
         if (canvas_count % 2 == 1) {
             console.log("canvas_count % 2 == 1");
@@ -24,6 +42,7 @@ class Canvases {
         }
         else {
             new_canvas.style.left = "-100px";
+            this.StyleDeclarationSetTransform(style, "scale(-1.0,1.0)");
         }
         var top = 0;
         if (canvas_count < 2) {
