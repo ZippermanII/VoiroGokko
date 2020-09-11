0f75e24adda289ed4a5f9e60c5dd99c62a18a748
ZippermanII <ababa@ex.com>
2020-09-12 02:03:38 +0900

    表情の自動切換え即席版

--- CommentParse.js
+++ CommentParse.js
@@ -6,8 +6,14 @@ function commentParse(comment,speaker,index){
     this.emotion = null;
     this.talkStr = comment;
     this.command = null;
+    var lastChara = comment[comment.length - 1]
+    if (lastChara == "。"){
+        this.talkStr = comment.slice(0,comment.length - 1)
+        lastChara = this.talkStr[this.talkStr.length - 1]
+    }
     var baseCommandStart = comment.lastIndexOf("##");
     var artists =[];
+    var randomEmote = ["000","004"];
     if(baseCommandStart != -1){
         this.talkStr = comment.slice(0,baseCommandStart);
         this.command = comment.slice(baseCommandStart + 2 , comment.length);
@@ -38,64 +44,19 @@ function commentParse(comment,speaker,index){
                 }
                 break;
             }
+        }
     }
+    else if(lastChara == "ｗ"||lastChara == "草"||lastChara == "w"){//これもっとスマートな書き方ないだろうか
+        this.command = "001";
+        this.emotion = "001";
+        return;
+    }
+    else{
+        this.emotion = randomEmote[ Math.floor( Math.random() * 2 ) ];
+        this.command = this.emotion;
+        return;
     }
 }
-// function commentParse(comment,speaker,index){
-//     this.speakerName = null;
-//     this.artist = "";
-//     this.artSeries = "";
-//     this.dress = "a";
-//     this.emotion = null;
-//     this.talkStr = comment;
-//     this.command = null;
-//     var baseCommandStart = comment.lastIndexOf("##");
-//     var artists =[];
-//     if(baseCommandStart != -1){
-//         this.talkStr = comment.slice(0,baseCommandStart);
-//         this.command = comment.slice(baseCommandStart + 2 , comment.length);
-//         var commandLastChara = this.command.slice(-1);
-//         var block1start = 0;
-//         var block2start = 0;
-//         var block3start = this.command.length - 3;
-//         var block4start = this.command.length;
-//         if(!this.command.match(/[^0-9]/gi)){
-//             this.emotion = this.command;
-//             return;
-//         }
-//         else if (!commandLastChara.match(/[^A-Z]/)) {//絵師名の最後がアルファベットだと不具合の恐れ要修正
-//             block3start -= 1;
-//             block4start -= 1;
-//             this.dress = commandLastChara;
-//         }
-//         for(key in speaker){
-//             if(this.command.startsWith(key)){
-//                 this.speakerName = key;
-//                 block2start = key.length;
-//                 block2 = this.command.slice(block2start, block3start)
-//                 console.log(block2);
-//                 if(block2.match(/[^0-9]/gi)){
-//                     this.artSeries = "series" + this.command.slice(block3start, block4start);
-//                 }
-//                 else{
-//                     block2 = this.command.slice(block2start, block4start)
-//                     this.artSeries = "series000"
-//                 }
-//                 this.artist = block2 + "様";
-//                 if (this.command == key) {
-//                     index = index.getElementsByTagName(key);
-//                     index = Array.prototype.slice.call(index);
-//                     index.forEach(element => {
-//                         artists.push(element.parentNode.nodeName)
-//                     });
-//                     this.artist = artists[ Math.floor( Math.random() * artists.length ) ] ;
-//                     this.artSeries = "series000";           
-//                 }
-//                 break;
-//             }
-//     }
-//     }
-// }
 commentParse.prototype.getSpeakerName = function () {
     return this.speakerName;
 };
