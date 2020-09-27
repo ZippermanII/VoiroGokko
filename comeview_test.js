42962697ad8050e96883fb417f6a3ae40e0055b6
ZippermanII <ababa@ex.com>
2020-09-28 06:21:49 +0900

    config.jsonに接続成功

--- comeview_test.js
+++ comeview_test.js
@@ -1,6 +1,6 @@
 async function Comment(){
  console.log("hello");
  var hostURL = "http://localhost/";
- talkerEffectsJSON = await fetch(hostURL + 'ignore/talker_effects.json',{"method":"GET"});
- effectsJSON = await talkerEffectsJSON.json();
+ configJSON = await fetch(hostURL + 'ignore/config.json',{"method":"GET"});
+ configJSON = await configJSON.json();
 };
