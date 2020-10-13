014547f326a9f5608fc1611013fa1526240e3d1e
ZippermanII <ababa@ex.com>
2020-10-13 17:30:11 +0900

    きりたんの「じゃん」発話を「じょん」に変更する仕組み実装

--- SpeakString.js
+++ SpeakString.js
@@ -1,6 +1,25 @@
 // JavaScript source code
 
-function speakString(str,speakerNum,speakStringJSON) {
+function speakString(str,speakerNum,speakStringJSON) {    
+    if (speakerNum == "2002"){
+        var kiritanReplace = {"じゃん":"じょん","ジャン":"ジョン"};
+        Object.keys(kiritanReplace).forEach(element => {
+            if (str.match(new RegExp(element, 'gi'))) {
+                var replaceTarget = element;
+                var replaceStr = kiritanReplace[element];
+                str = str.replace(new RegExp(replaceTarget, 'gi'), replaceStr); 
+                console.log("######################  " + str);
+                return str;
+            }
+        });
+        // if(str.match(/じゃん/gi)){
+        //     var replaceTarget = "じゃん";
+        //     var replaceStr = "じょん";
+        //     str = str.replace(new RegExp(replaceTarget, 'gi'), replaceStr); 
+        //     console.log("######################  " + str);
+        //     return str;
+        // }
+    }
     var stringArray = speakStringJSON[speakerNum];
     if(!stringArray){
         return str;
